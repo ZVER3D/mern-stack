1480e2bc6fa1e6cfc130497f48b62464ea1efc0c
Ivan <ivanakazver3d@gmail.com>
2018-07-29 23:23:39 +0700

    Add User Model, POST api/users/register and POST api/users/login routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -17,7 +22,7 @@ mongoose.connect(db, {
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.send('Hello'));
 
 // Use routs
 app.use('/api/users', users);
