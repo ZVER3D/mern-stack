c54e5633548312284d0da40b27856ab8f572c808
Ivan <ivanakazver3d@gmail.com>
2018-08-02 20:05:22 +0700

    Add register and login forms

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        errors: {}
+    };
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const newUser = {
+            email: this.state.email,
+            name: this.state.name,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+        console.log(newUser);
+    }
+
     render() {
+        // TODO: Make custom inputs with their own change events, to reduce code by like 3 times
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Sign Up</h1>
+                            <p className="lead text-center">Create your DevConnector account</p>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input type="text" 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Name" 
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.onChange}
+                                        required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="email" 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Email Address" 
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                        required />
+                                    <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Password" 
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                        required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Confirm Password" 
+                                        name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange}
+                                        required />
+                                </div>
+                                <input type="submit" className="btn btn-info btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
