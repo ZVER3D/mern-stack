8a90e227298ef046875ab3c85c6fb85d2809c20f
Ivan <ivanakazver3d@gmail.com>
2018-08-06 23:05:20 +0700

    Add some dashboard functionality and spinner component

---
+++ client/src/components/common/spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './spinner.css';
+
+const spinner = () => (
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+);
+
+export default spinner;
